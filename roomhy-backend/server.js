ec9c861a74ac55804a8af66f48b96083c45d1a2e
Yasmine Fathima <yasminefathima0401@gmail.com>
2026-01-05 08:44:43 +0530

    changes

--- roomhy-backend/server.js
+++ roomhy-backend/server.js
@@ -131,7 +131,7 @@ io.on('connection', (socket) => {
 });
 
 // Start Server with Socket.IO
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 10;
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
