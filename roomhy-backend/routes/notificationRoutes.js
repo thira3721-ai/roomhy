73dc26b3e8dc2697127fd362aacf63320efde953
thira3721-ai <thira3721@gmail.com>
2026-01-18 17:03:53 +0530

    Delete roomhy-backend directory

--- roomhy-backend/routes/notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const notificationController = require('../controllers/notificationController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/', protect, notificationController.listNotifications);
-
-module.exports = router;
